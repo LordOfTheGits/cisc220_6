e3ae53809d9200347c2b615bde8bd7d1607d87fa
mitch2446 <mitchellreid2468@gmail.com>
2016-12-13 21:26:18 -0500

    V 1.4 - Made changes in starting of the game

--- RacingArena.c
+++ RacingArena.c
@@ -9,57 +9,57 @@ int winner = 0;
 
 void *userCar(void *string) {
     while (carPosition[1]<40){
-	    if ( getchar() == '\n') {
-	    		carPosition[1]+= 1;
-	    }
+        if ( getchar() == '\n') {
+                carPosition[1]+= 1;
+        }
     }
     pthread_exit(NULL);
 }
 
 void *computerCar2(void *string){
-	
-	while (carPosition[2]<40){
-		carPosition[2] += 1;
-		usleep(1000*(rand()%100));
-	}
+    
+    while (carPosition[2]<40){
+        carPosition[2] += 1;
+        usleep(1000*(rand()%100));
+    }
 pthread_exit(NULL);
 }
 
 void *computerCar3(void *string){
-	
-	while (carPosition[3]<40){
-		carPosition[3] += 1;
-		usleep(1000*(rand()%100));
-	}
+    
+    while (carPosition[3]<40){
+        carPosition[3] += 1;
+        usleep(1000*(rand()%100));
+    }
 pthread_exit(NULL);
 }
 
 void *computerCar4(void *string){
-	
-	while (carPosition[4]<40){
-		carPosition[4] += 1;
-		usleep(1000*(rand()%100));
-	}
+    
+    while (carPosition[4]<40){
+        carPosition[4] += 1;
+        usleep(1000*(rand()%100));
+    }
 pthread_exit(NULL);
 }
 
 void *computerCar5(void *string){
-	
-	while (carPosition[5]<40){
-		carPosition[5] += 1;
-		usleep(1000*(rand()%100));
-	}
+    
+    while (carPosition[5]<40){
+        carPosition[5] += 1;
+        usleep(1000*(rand()%100));
+    }
 pthread_exit(NULL);
 }
 
 void *printThread(void *threadid){
 
-	while (1){
-	    system("clear");
+    while (1){
+        system("clear");
         printf("Welcome to Cisc220 Racing Arena\n");
         printf("Hit Enter to move forward \n");
         for (int i = 1; i < 6; i++){
-        	int x;
+            int x;
             for (x = 0; x < carPosition[i]; x++) {
                 printf("~");
             }
@@ -68,26 +68,34 @@ void *printThread(void *threadid){
                 printf(" ");
             }
             printf("# Line %d #\n", i);
-    	    if (carPosition[i] == 40){
-		        winner=i;
+            if (carPosition[i] == 40){
+                winner=i;
             }
         }
         if (winner){
-        	printf("Player %d Wins!\n", winner);
-        	pthread_exit(NULL);
+            printf("Player %d Wins!\n", winner);
+            pthread_exit(NULL);
         }
         usleep(133337);
     }
-}	
+}   
 
 int main(){
-	pthread_t run;
-    pthread_create(&run, NULL, userCar, NULL);
-    pthread_create(&run, NULL, computerCar2, NULL);
-    pthread_create(&run, NULL, computerCar3, NULL);
-    pthread_create(&run, NULL, computerCar4, NULL);
-    pthread_create(&run, NULL, computerCar5, NULL);
+
+    printf("Welcome to Cisc220 Racing Arena\n");
+    printf("Hit Enter to move forward \n");
+    pthread_t run;
     pthread_create(&run, NULL, printThread, NULL);
-    pthread_join(run, NULL);
-    exit(0);
+        
+    if ( getchar() == '\n') 
+    {
+        pthread_create(&run, NULL, userCar, NULL);
+        pthread_create(&run, NULL, computerCar2, NULL);
+        pthread_create(&run, NULL, computerCar3, NULL);
+        pthread_create(&run, NULL, computerCar4, NULL);
+        pthread_create(&run, NULL, computerCar5, NULL);
+        pthread_create(&run, NULL, printThread, NULL);
+        pthread_join(run, NULL);
+        exit(0);
+    }
 }
